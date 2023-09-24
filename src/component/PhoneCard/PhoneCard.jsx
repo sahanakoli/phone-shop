5e7ff26a0fc04f6564fc2232bf44def6dd216e2b
Sahana Koli <sahanasultanakoli@gmail.com>
2023-09-24 16:17:22 +0600

    handle add favorites and show favorite phone

--- src/component/PhoneCard/PhoneCard.jsx
+++ src/component/PhoneCard/PhoneCard.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unknown-property */
 import { useEffect, useState } from "react";
-import { Link, useLoaderData, useParams } from "react-router-dom";
+import {  useLoaderData, useParams } from "react-router-dom";
+import FavoritesPhone from "../FavoritesPhone/FavoritesPhone";
+
 
 
 
@@ -18,59 +22,12 @@ const PhoneCard = () => {
         setPhoneCard(findPhoneCard);
     
     },[id, phones]);
-    console.log(phoneCard);
     
-    const { image, phone_name, brand_name } = phoneCard
-
-
+ 
+    
     return (
-        <div className=" mt-8  flex justify-center  items-center">
-            <div className="relative flex w-6/12 max-w-[48rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-  <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
-    <img
-      src={image}
-      alt="image"
-      className="h-full w-full object-cover"
-    />
-  </div>
-  <div className="p-4">
-    <h6 className="mb-4 mt-5 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
-      {brand_name}
-    </h6>
-    <h4 className="mb-2 mt-5 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-      {phone_name}
-    </h4>
-    
-    <a className="inline-block" href="#">
-      <Link to="/favorites">
-      <button
-        className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
-        Add To Favorites
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          // eslint-disable-next-line react/no-unknown-property
-          stroke-width="2"
-          stroke="currentColor"
-          aria-hidden="true"
-          className="h-4 w-4"
-        >
-          <path
-            // eslint-disable-next-line react/no-unknown-property
-            stroke-linecap="round"
-            // eslint-disable-next-line react/no-unknown-property
-            stroke-linejoin="round"
-            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-          ></path>
-        </svg>
-      </button>
-      </Link>
-    </a>
-  </div>
-</div>
+        <div>
+           <FavoritesPhone phoneCard={phoneCard}></FavoritesPhone>
         </div>
     );
 };
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useState } from "react";
+
+import FavoritesPhone from "../../component/FavoritesPhone/FavoritesPhone";
 
 
 const Favorites = () => {
+
+     const [favorites, setFavorites] = useState([])
+    
+     const [noFund, setNoFund] = useState(false)
+
+     useEffect(() =>{
+        const favoriteItems = JSON.parse(localStorage.getItem('favorites'))
+
+        if(favoriteItems){
+            setFavorites(favoriteItems);
+        }
+        else{
+           
+            setNoFund('no data fund');
+        }
+     },[])
+
+     console.log(favorites)
     return (
         <div>
-            favorites
+            {noFund ? (<p className=" h-[80vh] flex items-center justify-center">{noFund}</p>) : 
+            (<div>
+                {favorites.length > 0 && <button className="px-5 mt-6 bg-green-200 block mx-auto rounded-md">Deleted All Favorites</button>}
+              <div className=" grid grid-cols-2 gap-5">
+                {favorites.map(phoneCard => <FavoritesPhone key={phoneCard.id} phoneCard={phoneCard}></FavoritesPhone>)}
+              </div>
+            </div>) }
         </div>
     );
 };
